6db60be1cbe12a1e0beefb196878806e3fb931d2
kulvinu <kulanikag2017@gmail.com>
2022-05-15 23:46:37 +0530

    K8s changes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.json({ limit: "30mb" }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 const URI = process.env.MONGODB_URI;
 
 mongoose
@@ -24,7 +24,7 @@ mongoose
     console.log("DB Connection Failed - " + err);
   });
 
-app.get("/api/test", (req,res) => {res.send("Hello From TEST endpoint!")})  
+app.get("/test", (req,res) => {res.send("Hello From TEST endpoint!")})  
 app.use("/api",customerRoutes);
 
 //event loop for server
